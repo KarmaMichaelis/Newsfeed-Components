22c61e0f59b665e0ad78bbe672ccc4dbf36a1030
Karma Michealis <achantayd@gmail.com>
2020-05-06 16:51:52 -0400

    function set

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,10 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function articleMaker(articleData){
+  const {title, date, firstParagraph,secondParagraph,thirdParagraph} =articleData
+  const article=document.createElement('div')
+  const articleTitle=document.createElement('h3')
+  const articleContent=document.createElement('p')
+  const articleButton=document.createElement('button')
+}
